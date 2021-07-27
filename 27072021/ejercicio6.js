42ee2dc1bea650a752fd6cceb80d4712cae4a14f
Anthony Ahumada <locopala99@gmail.com>
2021-07-27 10:29:41 +0200

    more practices while

---
+++ 27072021/ejercicio6.js
@@ -0,0 +1,11 @@
+//ciclo while
+
+//el while siempre se va a repetir hasta que la condicion se cumpla
+
+let anio = 2020;
+
+while(anio!=2020){
+    console.log("este mensaje se repetira hasta cumplir la condicion");
+}
+
+//en que caso es util el while?
