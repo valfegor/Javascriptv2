27c7b99156801edae53e281a36de195b743a9836
Anthony Ahumada <locopala99@gmail.com>
2021-07-27 08:47:44 +0200

    practicing for

---
+++ 27072021/ejercicio3.js
@@ -0,0 +1,21 @@
+//el ciclo FOR.
+
+
+//la funcion del for es repetir hasta que la condicion se cumpla.
+
+//estructura basica del for
+
+//consta de un indice desde donde inicia.
+
+//a la variable generalmente se le llama i , generalmente inicia desde 0 let i = 0;
+    
+//hasta donde va a trabajar un for el segundo parametro  i<3
+
+//el tercer valor es el incremento i++
+
+
+//for(let i ; i<3;i++);
+
+
+//ejemplo de alerts en un for.
+
