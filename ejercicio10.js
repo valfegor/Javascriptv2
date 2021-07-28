a5b1162e31271af41829a36f80e503014fb01ad7
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 08:50:21 +0200

    more

---
+++ ejercicio10.js
@@ -0,0 +1,8 @@
+//JSON - OBJETOS.
+
+
+//anteriormente se creaba algo llamado MVC = modelo vista controlador.
+
+//los usuarios estan en la vista la parte front (lo que el usuario ve) , views formularios.
+
+//
