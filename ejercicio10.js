9defb4dc47995bdea8b539e2e359960e694e063f
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 14:57:45 +0200

    practicing

--- ejercicio10.js
+++ ejercicio10.js
@@ -5,4 +5,62 @@
 
 //los usuarios estan en la vista la parte front (lo que el usuario ve) , views formularios.
 
-//
+//DATABASE , BACKEND  ,FRONT.
+
+//api = aplication programming interface , el web service es un servicio , se divide todo
+//se divide la vista del backend 
+
+
+//postman = permite ver esos json , la informacion que se envia y que trae las Apis , las apis de momento son URL.
+
+
+//Esto es un usuario
+
+let usuario = {
+    nombre:"Anthony",
+    edad:25,
+    documento:"1010228596",
+    casado:false,
+    user:"Valfegor",
+    password:"821996as"
+}
+
+//objeto dentro de otro objeto.
+
+let usuario = {
+    nombre:"Anthony",
+    edad:25,
+    documento:"1010228596",
+    casado:false,
+    user:"Valfegor",
+    password:"821996as",
+    rol:{
+        admin : true,
+        superadmin:false
+    }
+    
+}
+
+
+//tambien se pueden enviar funciones
+
+let usuario = {
+    nombre:"Anthony",
+    edad:25,
+    documento:"1010228596",
+    casado:false,
+    user:"Valfegor",
+    password:"821996as",
+    rol:{
+        admin : true,
+        superadmin:false
+    },
+    toString(){
+        alert("Hola amigo")
+    }
+
+    
+}
+
+//es mala practica tener funciones en el JSON.
+
