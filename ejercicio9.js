01952c12458a7647d3957501d644e90596a616a9
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 08:50:38 +0200

    more

---
+++ ejercicio9.js
@@ -0,0 +1,13 @@
+//optimizacion del if.
+
+let saludo =  "Hola";
+
+if(saludo==="hola"){
+    alert("saludaste bien");
+}
+
+
+//cuando tengo solo un if lo podemos optimizar en una sola linea
+
+
+if(saludo==="hola")alert("saludaste bien")
