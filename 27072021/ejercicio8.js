5d61b0d482fa4da484851ba48868918340aeae01
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 08:16:06 +0200

    more changes

---
+++ 27072021/ejercicio8.js
@@ -0,0 +1,31 @@
+//optimizaciones del if.
+
+let edad = prompt("ingresa tu edad");
+let terminos = prompt("Escriba Y para aceptar o N para rechazar");
+let resultado = false;
+
+//con un if normal
+
+if(terminos === "Y"){
+resultado = true;
+}else{
+resultado = false;
+}
+
+//en un switch.
+
+switch(terminos){
+    case "Y":
+        resultado = true;
+        break;
+    case "N":
+        resultado = false;
+    default:
+        break;
+    }
+
+
+//utilizando el operador ternario.
+
+(terminos==="Y")?resultado=true:resultado=false;
+
